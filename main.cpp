d528822a9e337a972fc9cb6a250e6938f129d0b4
seloibo13-beep <seloibo13@gmail.com>
2026-02-03 16:53:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,14 @@
 #include <windows.h>
-#include <thread>
 
-// Kebap Menu by Selim - Basit DLL İskeleti
-void HackThread() {
-    while (true) {
-        // Sağ Shift basılı mı kontrol et
-        if (GetAsyncKeyState(VK_RSHIFT) & 1) {
-            // Bir bip sesi çıkar ki çalıştığını anla
-            Beep(750, 300); 
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    }
-}
-
+// Bu kısım DLL'in giriş kapısıdır.
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
-    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
-        // Selim'e selam çak
-        MessageBoxA(NULL, "Kebap Menu by Selim Yuklendi!", "Basarili", MB_OK);
-        CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)HackThread, NULL, 0, NULL);
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        // Oyunun içine girdiği an bu bip sesini duyman lazım!
+        Beep(750, 500); 
+        // Bu kutu çıkmıyorsa enjeksiyon başarısızdır.
+        MessageBoxA(NULL, "Sistem Aktif: DLL Basariyla Enjekte Edildi!", "Selim Test", MB_OK | MB_ICONINFORMATION);
+        break;
     }
     return TRUE;
 }
